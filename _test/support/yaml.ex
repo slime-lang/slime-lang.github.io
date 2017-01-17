c8295f94ddfeb73033c6ade4ec0ce32ddd84482a
Anton Chuchkalov <a2new@yandex.ru>
2017-01-17 15:45:02 +0300

    update tests to match new reference file schema

--- _test/support/yaml.ex
+++ _test/support/yaml.ex
@@ -3,10 +3,19 @@ defmodule YAML do
     file
     |> :yamerl_constr.file
     |> hd
-    |> Enum.map(&to_map/1)
+    |> Enum.map(&to_string_map/1)
   end
 
-  defp to_map(proplist) do
+  def read_reference_examples do
+    "_data/reference.yml"
+      |> :yamerl_constr.file
+      |> hd
+      |> Enum.map(fn x -> Enum.into(x, %{}) end)
+      |> Enum.flat_map(fn section -> section['examples'] end)
+      |> Enum.map(&to_string_map/1)
+  end
+
+  defp to_string_map(proplist) do
     for {key, val} <- proplist, into: %{} do
       k = key |> to_string
       v = val |> to_string
